2bb42727d9205d1aad0f0969e3c223380bac6724
tokkaya79 <tokkaya79@gmail.com>
2023-06-07 22:35:24 +0300

    first commit

---
+++ wp-content/themes/jaxon/home.php
@@ -0,0 +1,307 @@
+<!-- 
+ Template Name: home
+
+  -->
+
+
+<!-- wp:group {"tagName":"main","align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}}} -->
+<main class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"48px","bottom":"48px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:48px;padding-bottom:48px"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">Hi, my name is <strong><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-ti-accent-color">Valentyna</mark></strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">I create websites and landing pages</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p id="tw-target-text">speed, functionality and support</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"fontSize":"normal"} -->
+<div class="wp-block-buttons has-custom-font-size has-normal-font-size"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="http://portfolio/wp-content/uploads/2023/05/tokarska_valentyna_CVen.pdf" target="_blank" rel="noreferrer noopener">Dowload CV (EN)</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:buttons {"fontSize":"normal"} -->
+<div class="wp-block-buttons has-custom-font-size has-normal-font-size"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Dowload CV (UK)</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:buttons {"fontSize":"normal"} -->
+<div class="wp-block-buttons has-custom-font-size has-normal-font-size"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Dowload CV (PL)</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"border":{"radius":{"topLeft":"160px","topRight":"0px","bottomLeft":"0px","bottomRight":"160px"}},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-ti-bg-alt-background-color has-background" style="border-top-left-radius:160px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:160px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"align":"center","id":123,"width":356,"height":582,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-large is-resized is-style-rounded"><img src="http://portfolio/wp-content/uploads/2023/05/valya3-625x1024.png" alt="" class="wp-image-123" width="356" height="582"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"40px","right":"var:preset|spacing|20","bottom":"40px","left":"var:preset|spacing|20"}}},"backgroundColor":"ti-accent","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-ti-accent-background-color has-background" style="padding-top:40px;padding-right:var(--wp--preset--spacing--20);padding-bottom:40px;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:image {"align":"center","id":21,"width":72,"height":72,"sizeSlug":"large","linkDestination":"custom","className":"is-resized"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="https://github.com/tokkaya79" target="_blank" rel="noreferrer noopener"><img src="http://portfolio/wp-content/uploads/2023/05/git.svg" alt="" class="wp-image-21" width="72" height="72"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"align":"center","id":20,"width":64,"height":64,"sizeSlug":"large","linkDestination":"custom","className":"is-resized"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="https://www.facebook.com/valentuna.tokarskaya" target="_blank" rel="noreferrer noopener"><img src="http://portfolio/wp-content/uploads/2023/05/fb.svg" alt="inst" class="wp-image-20" width="64" height="64"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"align":"center","id":22,"width":70,"height":70,"sizeSlug":"large","linkDestination":"custom","className":"is-resized"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="https://www.instagram.com/valentina.tokarskaya/" target="_blank" rel="noreferrer noopener"><img src="http://portfolio/wp-content/uploads/2023/05/inst.svg" alt="" class="wp-image-22" width="70" height="70"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"align":"center","id":32,"width":72,"height":72,"sizeSlug":"large","linkDestination":"custom","className":"is-resized"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="https://www.linkedin.com/in/valentina-tokarskaya-708502252/" target="_blank" rel="noreferrer noopener"><img src="http://portfolio/wp-content/uploads/2023/05/link.svg" alt="" class="wp-image-32" width="72" height="72"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"align":"center","id":23,"width":66,"height":66,"sizeSlug":"large","linkDestination":"custom","className":"is-resized"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="https://codepen.io/tokkaya79" target="_blank" rel="noreferrer noopener"><img src="http://portfolio/wp-content/uploads/2023/05/code.svg" alt="" class="wp-image-23" width="66" height="66"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px","right":"24px","left":"24px"},"blockGap":"0","margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"ti-bg-inv","textColor":"ti-fg-alt","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-ti-fg-alt-color has-ti-bg-inv-background-color has-text-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-right:24px;padding-bottom:64px;padding-left:24px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"40px","left":"40px"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="http://portfolio/wp-content/themes/jaxon/assets/img/icon-3.svg" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="wp-block-heading has-text-align-center"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-ti-accent-color">Get your own website in a few days!</mark></h4>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>I am an experienced freelancer specializing in creating websites from scratch</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>A clear definition of the goals, functionality and requirements for the site before starting work</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Professional approach and attention to detail</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Using ready-made solutions</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:image {"align":"center","width":57,"height":59,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://portfolio/wp-content/themes/jaxon/assets/img/icon-2.svg" alt="" width="57" height="59"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="wp-block-heading has-text-align-center" id="tw-target-text"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-ti-accent-color">I will translate your ideas into a website of the highest quality!</mark></h4>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Attractive and user oriented design</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Optimized download speed</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Responsive design (adaptability)</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>High quality content</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Integration with social networks and other platforms</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>SEO optimization</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:image {"align":"center","width":44,"height":56,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://portfolio/wp-content/themes/jaxon/assets/img/icon-1.svg" alt="" width="44" height="56"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="wp-block-heading has-text-align-center" id="tw-target-text"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-ti-accent-color">Reliable support and maintenance</mark></h4>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Backup and restore</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Providing technical support</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Responsive communication and updates</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Performance monitoring</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Making changes and updating content on the site</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Constant development of the site, including the addition of new features</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"40px"}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Last projects</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"0px"},"border":{"width":"0px","style":"none"}}} -->
+<div class="wp-block-column" style="border-style:none;border-width:0px"><!-- wp:image {"id":63,"width":559,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px"}}}} -->
+<figure class="wp-block-image size-full is-resized has-custom-border"><img src="http://portfolio/wp-content/uploads/2023/05/fast-delivery-edited-1.webp" alt="" class="wp-image-63" style="border-top-left-radius:8px;border-top-right-radius:8px" width="559"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"32px","right":"32px","bottom":"32px","left":"32px"}},"border":{"radius":{"bottomLeft":"8px","bottomRight":"8px"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-ti-bg-alt-background-color has-background" style="border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-top:32px;padding-right:32px;padding-bottom:32px;padding-left:32px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Fast delivery</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(for portfolio)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://portfolio/en/projects/fast-delivery/">See more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":64,"width":556,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px"}}}} -->
+<figure class="wp-block-image aligncenter size-full is-resized has-custom-border"><img src="http://portfolio/wp-content/uploads/2023/05/videographer-edited.webp" alt="" class="wp-image-64" style="border-top-left-radius:8px;border-top-right-radius:8px" width="556"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"32px","right":"32px","bottom":"32px","left":"32px"}},"border":{"radius":{"bottomLeft":"8px","bottomRight":"8px"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-ti-bg-alt-background-color has-background" style="border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-top:32px;padding-right:32px;padding-bottom:32px;padding-left:32px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">EZ Videographer</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(for commercial)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">See more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":69,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px"}}}} -->
+<figure class="wp-block-image aligncenter size-large has-custom-border"><img src="http://portfolio/wp-content/uploads/2023/05/bonfire-edited.webp" alt="" class="wp-image-69" style="border-top-left-radius:8px;border-top-right-radius:8px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"32px","right":"32px","bottom":"32px","left":"32px"}},"border":{"radius":{"bottomLeft":"8px","bottomRight":"8px"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-ti-bg-alt-background-color has-background" style="border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-top:32px;padding-right:32px;padding-bottom:32px;padding-left:32px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Bonfire Club</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(for commercial)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://portfolio/projects/bonfire-club/">See more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"0px"},"border":{"width":"0px","style":"none"}}} -->
+<div class="wp-block-column" style="border-style:none;border-width:0px"><!-- wp:image {"id":68,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px"}}}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="http://portfolio/wp-content/uploads/2023/05/ui-desk-edited.webp" alt="" class="wp-image-68" style="border-top-left-radius:8px;border-top-right-radius:8px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"32px","right":"32px","bottom":"32px","left":"32px"}},"border":{"radius":{"bottomLeft":"8px","bottomRight":"8px"}}},"backgroundColor":"ti-bg-alt","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-ti-bg-alt-background-color has-background" style="border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-top:32px;padding-right:32px;padding-bottom:32px;padding-left:32px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">UI desk</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>(for portfolio)</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://portfolio/projects/ui-desk/">See more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"100%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:buttons {"align":"full","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignfull"><!-- wp:button {"textAlign":"center","className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-text-align-center wp-element-button" href="http://portfolio/projects/">All projects ...</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
